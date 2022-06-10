aff0eb18f5a771267586c5f8af6161c3d7ee7bda
Gregg Roseker (Cazoo) <gregg.roseker@cazoo.co.uk>
2022-06-10 17:50:28 +0100

    changes to Login and Register pages

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,15 +3,15 @@ import "./login.css";
 export default function Login() {
   return (
     <div className="login">
-        <span className="loginTitle">Login</span>
-        <form className="loginForm">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm">
         <label>Email</label>
-        <input type="text" className="loginInput" placeholder="Enter your email..." />
+        <input className="loginInput" type="text" placeholder="Enter your email..." />
         <label>Password</label>
-        <input type="password" className="loginInput"placeholder="Enter your password..." />
+        <input className="loginInput" type="password" placeholder="Enter your password..." />
         <button className="loginButton">Login</button>
-        </form>
+      </form>
         <button className="loginRegisterButton">Register</button>
     </div>
-  )
+  );
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,19 +1,19 @@
-import "./register.css";
+import "./register.css"
 
 export default function Register() {
-  return (
-    <div className="register">
-        <span className="registerTitle">Register</span>
-        <form className="registerForm">
+    return (
+        <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm">
         <label>Username</label>
-        <input type="text" className="registerInput" placeholder="Enter your username..." />
+        <input className="registerInput" type="text" placeholder="Enter your username..." />
         <label>Email</label>
-        <input type="text" className="registerInput" placeholder="Enter your email..." />
+        <input className="registerInput" type="text" placeholder="Enter your email..." />
         <label>Password</label>
-        <input type="password" className="registerInput"placeholder="Enter your password..." />
+        <input className="registerInput" type="password" placeholder="Enter your password..." />
         <button className="registerButton">Register</button>
-        </form>
+      </form>
         <button className="registerLoginButton">Login</button>
     </div>
-  )
+    )
 }
